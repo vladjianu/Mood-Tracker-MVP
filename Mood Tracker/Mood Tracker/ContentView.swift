b0f35a9f4a3b3374555593b656f6a81d82e9826b
Vlad Jianu <vladjianu@192.168.1.19>
2024-12-23 17:08:20 +0200

    initial xcode changes

---
+++ Mood Tracker/Mood Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mood Tracker
+//
+//  Created by Vlad Jianu on 23/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mood Tracker/Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by Vlad Jianu on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
